4ec3277f25fc854cc50ab8f3a99ca79b0850a800
Arun Varadharajalu <arunvaradharajalu@gmail.com>
2024-07-09 03:01:23 -0400

    Add User manual

--- src/public/js/list-tasks.js
+++ src/public/js/list-tasks.js
@@ -99,6 +99,32 @@ $(async () => {
         	</tr>`);
 		});
 	});
+
+	// Get the modal
+	var modal = $("#user-manual-modal");
+
+	// Get the button that opens the modal
+	var btn = $("#user-manual");
+
+	// Get the <span> element that closes the modal
+	var span = $(".close");
+
+	// When the user clicks the button, open the modal 
+	btn.click(function() {
+		modal.show();
+	});
+
+	// When the user clicks on <span> (x), close the modal
+	span.click(function() {
+		modal.hide();
+	});
+
+	// When the user clicks anywhere outside of the modal, close it
+	$(window).click(function(event) {
+		if ($(event.target).is(modal)) {
+			modal.hide();
+		}
+	});
 });
 
 // Function to navigate to edit-task.html with task id as parameter
